5eedf5777965875d714a9c56d5f383fea4007763
Luke Stanley <luke.stanley@gmail.com>
2022-09-21 15:42:27 +0000

    Experiments with explicit control of block creation and output reactivity

--- src/app/components/blocks-workflow/blocks-workflow.component.ts
+++ src/app/components/blocks-workflow/blocks-workflow.component.ts
@@ -1,5 +1,8 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
-import {clone} from 'lodash-es';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ViewContainerRef } from '@angular/core';
+import { clone } from 'lodash-es';
+import { DebugBlockComponent } from '../../blocks/debug-block/debug-block.component';
+import { MappingBlockComponent } from '../../blocks/mapping-block/mapping-block.component';
 
 @Component({
   selector: 'app-blocks-workflow',
@@ -14,9 +17,52 @@ export class BlocksWorkflowComponent implements OnInit {
 
   @Output() workflowComplete = new EventEmitter();
 
-  constructor() { }
+  constructor(public viewContainerRef: ViewContainerRef) { }
 
   ngOnInit() {
+    // We dynamically add Angular components to the view using addBlock, popping them off the flow stack after yielding a subscription
+
+    let flow = [
+      { "component": MappingBlockComponent, "config": { "mapping": "uuid()" } },
+      { "component": DebugBlockComponent, "config": {} },
+    ]
+
+    function addBlock(parent, block, config, model) {
+      const component = parent.viewContainerRef.createComponent(block);
+      component.instance.config = config;
+      component.instance.model = model;
+      component.instance.ngOnChanges({}); // Force change now we have set the config and model inputs
+      return component;
+    }
+    // We could manually use like this:
+    // const mappingBlock = addBlock(this, MappingBlockComponent, {"mapping": "uuid()"}, {});
+    //console.dir(mappingBlock);
+
+    //mappingBlock.instance.output.subscribe((value) => {
+    //console.log(value);
+    //addBlock(this, DebugBlockComponent, {}, value);
+    // });
+
+    // But instead, we add  block components specified in the flow array, and after each component has output, add the next block if any, feeding the value from the last output:
+    let model = {}; // we start with no data output
+    let activeFlow = 0; // start with the first flow item (that uses MappingBlockComponent, with a specific config and empty model).
+    // while there are still flows to loop through, we add the next block, and subscribe to its output, which we then use to add the next block, and so on.
+    function nextFlow(parent) {
+      let component = flow[activeFlow].component;
+      let config = flow[activeFlow].config;
+      console.log("adding block", config)
+      let block = addBlock(parent, component, config, model);
+      block.instance.output.subscribe((value) => {
+        console.log("block output", value);
+        model = value;
+        activeFlow++;
+        if (activeFlow < flow.length) {
+          nextFlow(parent);
+        }
+      });
+      console.log("subscribing to block");
+    }
+    nextFlow(this);
   }
 
   updateModel(modelNumber, value) {
